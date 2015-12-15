a0985e519cececdfded93d1f8ad1fb0ac35f02cd
pl1ght <forsaken@4saken.org>
2015-12-15 08:05:19 -0600

    comments

--- ec2ctrl.py
+++ ec2ctrl.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 import boto3, logging
 
-
+# Setup Logging
 logger = logging.getLogger('ec2ctrl.py')
 hdlr = logging.FileHandler('/var/log/ec2ctrl.og')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -8,14 +10,18 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
+
+# .aws\credentials profiles to loop through
 profiles = ["imagingqa", "imagingdev", "mobiledev", "dataservicesdev", "dataservicesqa", "javastacksdev"]
 
+# Loop through profiles and perform shutdown
 for i in profiles:
     profile = i
     region = 'us-east-1'
     session = boto3.Session(profile_name = profile, region_name=region)
     ec2 = session.resource('ec2')
 
+# Loop through instances on each account to find all running instances to perform shutdown on
     instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     for instance in instances:
